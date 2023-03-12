e6d9acdcb70d075bec9d9956fe7bf829a73979e6
Petr Vavřínek <vavrinekpetr155@gmail.com>
2023-03-12 19:57:16 +0100

    feat: instruction task actor

--- Source/BachelorThesis/Private/Utils/GameUtils.cpp
+++ Source/BachelorThesis/Private/Utils/GameUtils.cpp
@@ -1,29 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Utils/GameUtils.h"
 #include "Kismet/GameplayStatics.h"
 #include "UObject/UObjectGlobals.h"
 #include "EngineMinimal.h"
 // #include "Kismet/BlueprintFunctionLibrary.h"
 
-void UGameUtils::PrintDebugString(const UObject* WorldContextObject, const FString& Content, FLinearColor Color, float Duration)
+void UGameUtils::PrintDebugString(const UObject *WorldContextObject, const FString &Content, FLinearColor Color, float Duration)
 {
 	const FString Prefix = WorldContextObject ? ("[" + WorldContextObject->GetName() + "] ") : "";
 	const FString Message = Prefix + Content;
 	GEngine->AddOnScreenDebugMessage(-1, Duration, Color.ToFColor(false), Message);
 }
 
-UObject* UGameUtils::K2_DuplicateObject(UObject* Object)
+UObject *UGameUtils::K2_DuplicateObject(UObject *Object)
 {
-	if (Object == nullptr) {
+	if (Object == nullptr)
+	{
 		UE_LOG(LogTemp, Warning, TEXT("Could not duplicate object, received nullptr"));
 		return nullptr;
 	}
 	return DuplicateObject(Object, Object->GetOuter());
 }
 
-bool UGameUtils::RenameObject(UObject* Object, const FString& NewName)
+bool UGameUtils::RenameObject(UObject *Object, const FString &NewName)
 {
 	if (Object == nullptr)
 		UE_LOG(LogTemp, Warning, TEXT("Could not rename object, received nullptr"));
@@ -31,11 +31,28 @@ bool UGameUtils::RenameObject(UObject* Object, const FString& NewName)
 	return Object->Rename(*NewName, nullptr, REN_ForceNoResetLoaders);
 }
 
-
-bool UGameUtils::GetLevelName(TSoftObjectPtr<UWorld> World, FString& LevelName)
+bool UGameUtils::GetLevelName(TSoftObjectPtr<UWorld> World, FString &LevelName)
 {
-	if (!World.IsValid())	return false;
+	if (!World.IsValid())
+		return false;
 	LevelName = FString(World->GetMapName());
 	LevelName.RemoveFromStart(World->StreamingLevelsPrefix);
 	return true;
+}
+
+TArray<AActor *> UGameUtils::GetAllActorsOfClass(AActor *ActorContext, TSubclassOf<AActor> ActorClass)
+{
+	if (ActorContext == nullptr)
+		return {};
+
+	const UWorld *World = ActorContext->GetWorld();
+	if (!World)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Cannot get world from %s", *ActorContext->GetName()));
+		return {};
+	}
+
+	TArray<AActor *> OutActors;
+	UGameplayStatics::GetAllActorsOfClass(ActorContext, ActorClass, OutActors);
+	return OutActors;
 }
--- Source/BachelorThesis/Public/Utils/GameUtils.h
+++ Source/BachelorThesis/Public/Utils/GameUtils.h
@@ -2,28 +2,32 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "Core.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameFramework/Actor.h"
 #include "GameUtils.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class BACHELORTHESIS_API UGameUtils : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
 public:
 	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject", DefaultToSelf = "WorldContextObject", CallableWithoutWorldContext, AdvancedDisplay = "2"))
-	static void PrintDebugString(const UObject* WorldContextObject, const FString& Content, FLinearColor Color = FLinearColor(0, 0.5f, 0.5f), float Duration = 1.f);
+	static void PrintDebugString(const UObject *WorldContextObject, const FString &Content, FLinearColor Color = FLinearColor(0, 0.5f, 0.5f), float Duration = 1.f);
 
-	UFUNCTION(BlueprintCallable, Category = "Game|Utils", DisplayName="DuplicateObject")
-	static UObject* K2_DuplicateObject(UObject* Object);
+	UFUNCTION(BlueprintCallable, Category = "Game|Utils", DisplayName = "DuplicateObject")
+	static UObject *K2_DuplicateObject(UObject *Object);
 
 	UFUNCTION(BlueprintCallable, Category = "Game|Utils")
-	static bool RenameObject(UObject* Object, const FString& NewName);
+	static bool RenameObject(UObject *Object, const FString &NewName);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Game|Utils")
-	static bool GetLevelName(TSoftObjectPtr<UWorld> World, FString& LevelName);
+	static bool GetLevelName(TSoftObjectPtr<UWorld> World, FString &LevelName);
+
+	UFUNCTION(BlueprintCallable, Category = "Game|Utils")
+	static TArray<AActor*> GetAllActorsOfClass(AActor *ActorContext, TSubClassOf<AActor> ActorClass);
 };
